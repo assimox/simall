f515686a15673e0dd7da9ce44421d11de0058b1b
Assim <mohammedassimboutrigui@gmail.com>
2026-03-12 12:50:06 +0000

    fix: Use raw dangerouslySetInnerHTML for Meta Pixel in head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,25 +29,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script dangerouslySetInnerHTML={{__html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1248423410735431');
+          fbq('track', 'PageView');
+        `}} />
+        <noscript dangerouslySetInnerHTML={{__html: `
+          <img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=1248423410735431&ev=PageView&noscript=1" />
+        `}} />
+      </head>
       <body className={`${inter.variable} ${playfair.variable}`}>
-        {/* Meta Pixel Code - raw script tag */}
-        <Script id="meta-pixel-base" strategy="beforeInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1248423410735431');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          <img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=1248423410735431&ev=PageView&noscript=1" alt="" />
-        </noscript>
         <LanguageProvider>
         <CartProvider>
           <div className="top-banner">
